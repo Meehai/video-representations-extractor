8ffd1682b2e95dfb096ccde863f739a2bbbc7130
Meehai <mihaicristianpirvu@gmail.com>
2021-05-19 03:20:08 +0300

    Working example

--- representations/dexined/__init__.py
+++ representations/dexined/__init__.py
@@ -1,24 +1,27 @@
 import numpy as np
 import torch as tr
 from nwdata.utils import fullPath
-from nwmodule.utilities import device
+from nwmodule.utilities import device, trModuleWrapper
 from media_processing_lib.image import imgResize, tryWriteImage
 from scipy.special import expit as sigmoid
 
 from ..representation import Representation
+# from .model import DexiNet as Model
+from matplotlib.cm import gray
 
 import sys
 sys.path.append(str(fullPath(__file__).parents[0] / "DexiNed"))
 from model_dexined import DexiNed as Model
 
 def preprocessImage(img):
-    img, coordinates = imgResize(img, height=512, width=512, mode="black_bars", imgLib="lycon", returnCoordinates=True)
+    img, coordinates = imgResize(img, height=352, width=352, mode="black_bars", imgLib="lycon", returnCoordinates=True)
     img = np.float32(img) / 255
     img = img.transpose(2, 0, 1)[None]
     return img, coordinates
 
 def postprocessImage(img, coordinates):
     img = np.array(img)
+    img = sigmoid(img)
     img[img < 0.0] = 0.0
     img = 1 - img
     img = img.mean(axis=0)[0][0]
@@ -28,19 +31,17 @@ def postprocessImage(img, coordinates):
 
 class DexiNed(Representation):
     def __init__(self, weightsFile:str):
-        self.model = Model().to(device)
-        self.model.load_state_dict(tr.load(weightsFile, map_location=device))
-        self.model.eval()
+        model = Model().to(device)
+        model.load_state_dict(tr.load(weightsFile, map_location=device))
+        self.model = trModuleWrapper(model)
 
     def make(self, frame):
         A, coordinates = preprocessImage(frame)
         with tr.no_grad():
-            B = self.model.forward(tr.from_numpy(A).to(device))
-            C = [tr.sigmoid(x) for x in B]
-            D = [x.cpu().numpy() for x in C]
-        E = postprocessImage(D, coordinates)
-        F = imgResize(E, height=frame.shape[0], width=frame.shape[1], onlyUint8=False)
-        return F
+            B = self.model.npForward(A)
+        C = postprocessImage(B, coordinates)
+        D = imgResize(C, height=frame.shape[0], width=frame.shape[1], onlyUint8=False)
+        return D
 
     def makeImage(self, x):
         return np.uint8(x * 255)
--- representations/dexined/setup.sh
+++ representations/dexined/setup.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/xavysp/DexiNed/
+git clone --depth 1 https://github.com/xavysp/DexiNed/
+
 mv DexiNed/model.py DexiNed/model_dexined.py
 xdg-open https://drive.google.com/u/0/uc?id=1MRUlg_mRwDiBiQLKFVuEfuvkzs65JFVe
