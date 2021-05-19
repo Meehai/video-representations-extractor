e3c50361939e26ef3da89bb7d98b93296f8e58a7
Meehai <mihaicristianpirvu@gmail.com>
2021-05-19 09:49:49 +0300

    Updated collage making script

--- main_make_collage.py
+++ main_make_collage.py
@@ -1,5 +1,16 @@
 from main import getArgs, tryReadVideo, getRepresentation, np, trange, tryWriteImage
 
+def suffix(x:int):
+    x = str(x)
+    if not x[-1] in ("1", "2", "3") or (len(x) > 1 and x[-2] == "1"):
+        return "th"
+    elif x[-1] == "1":
+        return "st"
+    elif x[-1] == "2":
+        return "nd"
+    else:
+        return "rd"
+
 def main():
     args = getArgs()
     assert args.outputDir.exists()
@@ -10,7 +21,11 @@ def main():
     (args.outputDir / "collage").mkdir(exist_ok=True)
     for i in trange(len(video)):
         inPaths = ["%s/%s/%d.npz" % (args.outputDir, name, i) for name in names]
-        outputs = [np.load(x)["arr_0"] for x in inPaths]
+        try:
+            outputs = [np.load(x)["arr_0"] for x in inPaths]
+        except Exception as e:
+            print("[main_make_collage] Error loading %d%s npz file. Ending prematurely." % (i + 1, suffix(i + 1)))
+            break
         pngs = [r.makeImage(o) for r, o in zip(representations, outputs)]
         png = np.concatenate(pngs, axis=1)
         tryWriteImage(png, "%s/collage/%d.png" % (args.outputDir, i))
