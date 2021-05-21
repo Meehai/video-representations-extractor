444ac18f9d317d16c594e3d72e82f410c6b3ba4c
Mihai Cristian Pirvu <mihaicristianpirvu@gmail.com>
2021-05-21 11:15:23 +0300

    Updated a little the code

--- representations/depth_jiaw/depth_jiaw.py
+++ representations/depth_jiaw/depth_jiaw.py
@@ -1,7 +1,9 @@
 import numpy as np
 import torch
 from skimage.transform import resize as imresize
+from media_processing_lib.image import imgResize
 from nwmodule.utilities import device
+from matplotlib.cm import hot
 
 from .DispResNet import DispResNet
 from ..representation import Representation
@@ -9,8 +11,9 @@ from ..representation import Representation
 def preprocessImage(img, size=None, multiples=(64, 64), device=torch.device('cuda')):
     if size is None:
         size = img.shape[:2]
+    img = imgResize(img, height=256, width=832)
     img = img.astype(np.float32)
-    img = imresize(img, closest_fit(size, multiples))
+    # img = imresize(img, closest_fit(size, multiples))
     img = np.transpose(img, (2, 0, 1))
     img = ((torch.from_numpy(img).unsqueeze(0) / 255 - 0.45) / 0.225).to(device)
     return img
@@ -29,8 +32,7 @@ def closest_fit(size, multiples):
     return [round(size[i] / multiples[i]) for i in range(len(multiples))]
 
 class DepthJiaw(Representation):
-    # def __init__(self, weightsFile:str, resNetLayers:int):
-    def __init__(self):
+    def __init__(self, weightsFile:str, resNetLayers:int):
         model = DispResNet().to(device)
         # model = DispResNet(resNetLayers, False).to(device)
         # weights = torch.load(weightsFile, map_location=device)
@@ -41,15 +43,15 @@ class DepthJiaw(Representation):
 
     def make(self, x):
         x_ = preprocessImage(x, multiples=self.multiples, device=device)
-        breakpoint()
         with torch.no_grad():
             y = self.model(x_)
         y = postprocessImage(y, x.shape[:2])
         return y
 
     def makeImage(self, x):
-        # TODO wat dis
-        return x
+        y = hot(x)[..., 0 : 3]
+        y = np.uint8(y * 255)
+        return y
 
     def setup(self):
         pass
