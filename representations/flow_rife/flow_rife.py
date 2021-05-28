0a0e00442a8e471f4f1e3dd446eef9555e401f59
Meehai <mihaicristianpirvu@gmail.com>
2021-05-28 15:47:26 +0300

    Fixed RIFE

--- representations/flow_rife/flow_rife.py
+++ representations/flow_rife/flow_rife.py
@@ -2,9 +2,11 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import flow_vis
+from typing import Dict
 from torchvision import transforms
 from nwmodule.utilities import device
 from media_processing_lib.image import imgResize
+from media_processing_lib.video import MPLVideo
 
 from ..representation import Representation
 from .RIFE_HDv2 import Model
@@ -20,16 +22,13 @@ class FlowRife(Representation):
 		self.UHD = False
 		self.no_backward_flow = True if computeBackwardFlow is None else not computeBackwardFlow
 
-	def make(self, video, t):
+	def make(self, video:MPLVideo, t:int, depenedencyInputs:Dict[str, np.ndarray]) -> np.ndarray:
 		frame1 = video[t]
 		frame2 = video[t + 1] if t < len(video) - 2 else frame1.copy()
-		# frame1 = imgResize(frame1, width=256, height=256)
-		# frame2 = imgResize(frame2, width=256, height=256)
-		frame1 = (np.float32(frame1) / 255).transpose(2, 0, 1)[None]
-		frame2 = (np.float32(frame2) / 255).transpose(2, 0, 1)[None]
-
-		I0 = torch.from_numpy(frame1).to(device, non_blocking=True)
-		I1 = torch.from_numpy(frame1).to(device, non_blocking=True)
+		
+		# Convert, preprocess & pad
+		I0 = torch.from_numpy(np.transpose(frame1, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
+		I1 = torch.from_numpy(np.transpose(frame2, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
 		n, c, h, w = I0.shape
 		ph = ((h - 1) // 32 + 1) * 32
 		pw = ((w - 1) // 32 + 1) * 32
@@ -37,9 +36,16 @@ class FlowRife(Representation):
 		I0 = F.pad(I0, padding)
 		I1 = F.pad(I1, padding)
 
-		flow = self.model.inference(I0, I1)
-		# flow = self.model.inference(I0, I1, self.UHD, self.no_backward_flow)
+		flow = self.model.inference(I0, I1, self.UHD, self.no_backward_flow)
+
+		# Convert, postprocess and remove pad
 		flow = flow[0].cpu().numpy().transpose(1, 2, 0)
+		returnedShape = flow.shape[0 : 2]
+		# Remove the padding to keep original shape
+		halfPh, halfPw = (ph - h) // 2, (pw - w) // 2
+		flow = flow[0 : returnedShape[0]-halfPh, 0 : returnedShape[1]-halfPw]
+		# [-px : px] => [-1 : 1]
+		flow /= returnedShape
 		# [-1 : 1] => [0 : 1]
 		flow = (flow + 1) / 2
 		return flow
