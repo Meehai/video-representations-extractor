050c940683e7cd6528332c01f13d61a22fa87c20
Mihai Cristian Pirvu <mihaicristianpirvu@gmail.com>
2022-04-20 13:01:56 +0300

    one more weights dir update

--- ci_imgur/ci.sh
+++ ci_imgur/ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+test -e imgur.sh || wget https://raw.githubusercontent.com/tremby/imgur.sh/main/imgur.sh -O imgur.sh # imgur upload script
+chmod +x imgur.sh
 test -e DJI_0956_720p.MP4 || gdown https://drive.google.com/uc?id=158U-W-Gal6eXxYtS1ca1DAAxHvknqwAk # video
 mkdir -p ../weights
 cd ../weights
@@ -8,6 +10,6 @@ test -e safeuav_semantic_0956.pkl || gdown https://drive.google.com/uc?id=1tgni1
 cd -
 test -e cfg.yaml || wget https://pastebin.com/raw/D0awQLZP -O cfg.yaml # cfg
 X=$(shuf -i 1-9000 -n 1)
-python3 ../scripts/main.py --videoPath DJI_0956_720p.MP4 --cfgPath cfg.yaml --outputDir test_imgur/ --outputResolution 240,426 --startFrame $X --endFrame $((X+1))
+python3 ../scripts/main.py --videoPath DJI_0956_720p.MP4 --cfgPath cfg.yaml --outputDir ../ci_imgur/test_imgur/ --outputResolution 240,426 --startFrame $X --endFrame $((X+1))
 convert -resize 50% test_imgur//collage/$X.png test_imgur//collage/$X.png
 bash imgur.sh test_imgur//collage/$X.png
--- video_representations_extractor/representations/semantic/safeuav/sseg_safeuav.py
+++ video_representations_extractor/representations/semantic/safeuav/sseg_safeuav.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import torch as tr
 from typing import List
@@ -13,59 +14,60 @@ from ...representation import Representation, RepresentationOutput
 device = tr.device("cuda") if tr.cuda.is_available() else tr.device("cpu")
 
 class _RGB(RGB):
-	def get_encoder(self, outputNode):
-		return EncoderMap2Map(dIn=self.num_dims)
+    def get_encoder(self, outputNode):
+        return EncoderMap2Map(dIn=self.num_dims)
 
-	def get_decoder(self, inputNode):
-		pass
+    def get_decoder(self, inputNode):
+        pass
 
 class _Semantic(Semantic):
-	def get_encoder(self, outputNode):
-		pass
+    def get_encoder(self, outputNode):
+        pass
 
-	def get_decoder(self, inputNode):
-		return DecoderMap2Map(dOut=self.num_dims)
+    def get_decoder(self, inputNode):
+        return DecoderMap2Map(dOut=self.num_dims)
 
 
 class SSegSafeUAV(Representation):
-	def __init__(self, numClasses:int, trainHeight:int, trainWidth:int, colorMap:List, weightsFile:str, **kwargs):
-		super().__init__(**kwargs)
-		assert len(colorMap) == numClasses, f"{colorMap} ({len(colorMap)}) vs {numClasses}"
-		assert Path(weightsFile).exists(), f"Weights file '{weightsFile}' does not exist."
-		self.model = None
-		self.numClasses = numClasses
-		self.weightsFile = weightsFile
-		self.colorMap = colorMap
-		self.trainHeight = trainHeight
-		self.trainWidth = trainWidth
+    def __init__(self, numClasses:int, trainHeight:int, trainWidth:int, colorMap:List, weightsFile:str, **kwargs):
+        super().__init__(**kwargs)
+        assert len(colorMap) == numClasses, f"{colorMap} ({len(colorMap)}) vs {numClasses}"
+        weightsFile = Path(f"{os.environ['VRE_WEIGHTS_DIR']}/{weightsFile}").absolute()
+        assert Path(weightsFile).exists(), f"Weights file '{weightsFile}' does not exist."
+        self.model = None
+        self.numClasses = numClasses
+        self.weightsFile = weightsFile
+        self.colorMap = colorMap
+        self.trainHeight = trainHeight
+        self.trainWidth = trainWidth
 
-	@overrides
-	def make(self, t: int) -> RepresentationOutput:
-		frame = np.array(self.video[t])
-		img = image_resize(frame, height=self.trainHeight, width=self.trainWidth, interpolation="bilinear")
-		img = np.float32(frame[None]) / 255
-		tr_img = tr.from_numpy(img).to(device)
-		with tr.no_grad():
-			tr_res = self.model.forward(tr_img)[0]
-		np_res = tr_res.to("cpu").numpy()
-		res = np.argmax(np_res, axis=-1).astype(np.uint8)
-		return res
-	
-	@overrides
-	def makeImage(self, x: RepresentationOutput) -> np.ndarray:
-		newImage = np.zeros((*x["data"].shape, 3), dtype=np.uint8)
-		for i in range(self.numClasses):
-			newImage[x["data"] == i] = self.colorMap[i]
-		return newImage
+    @overrides
+    def make(self, t: int) -> RepresentationOutput:
+        frame = np.array(self.video[t])
+        img = image_resize(frame, height=self.trainHeight, width=self.trainWidth, interpolation="bilinear")
+        img = np.float32(frame[None]) / 255
+        tr_img = tr.from_numpy(img).to(device)
+        with tr.no_grad():
+            tr_res = self.model.forward(tr_img)[0]
+        np_res = tr_res.to("cpu").numpy()
+        res = np.argmax(np_res, axis=-1).astype(np.uint8)
+        return res
+    
+    @overrides
+    def makeImage(self, x: RepresentationOutput) -> np.ndarray:
+        newImage = np.zeros((*x["data"].shape, 3), dtype=np.uint8)
+        for i in range(self.numClasses):
+            newImage[x["data"] == i] = self.colorMap[i]
+        return newImage
 
-	@overrides
-	def setup(self):
-		if not self.model is None:
-			return
+    @overrides
+    def setup(self):
+        if not self.model is None:
+            return
 
-		rgbNode = _RGB(name="rgb")
-		semanticNode = _Semantic(name="semantic", semantic_classes=self.numClasses, semantic_colors=self.colorMap)
-		model = SingleLink(rgbNode, semanticNode)
-		model.load_state_dict(tr.load(self.weightsFile))
-		model.to(device)
-		self.model = model
+        rgbNode = _RGB(name="rgb")
+        semanticNode = _Semantic(name="semantic", semantic_classes=self.numClasses, semantic_colors=self.colorMap)
+        model = SingleLink(rgbNode, semanticNode)
+        model.load_state_dict(tr.load(self.weightsFile))
+        model.to(device)
+        self.model = model
