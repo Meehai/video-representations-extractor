38eedf7f1cdea5779c30bb7ed4a2e572d40ec8ab
Mihai Cristian Pirvu <mihaicristianpirvu@gmail.com>
2023-10-31 19:53:32 +0200

    dpt

--- test/integration/vre/test_vre_batched.py
+++ test/integration/vre/test_vre_batched.py
@@ -1,10 +1,11 @@
-import gdown
-import pims
-import numpy as np
 import shutil
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from datetime import datetime
+import gdown
+import pims
+import numpy as np
+import torch as tr
 
 from vre import VRE
 from vre.representations import build_representations_from_cfg
@@ -19,16 +20,18 @@ def setup():
 def test_vre_batched():
     video_path = setup()
     video = pims.Video(video_path)
+    device = "cuda" if tr.cuda.is_available() else "cpu"
     representations_dict = {
         "rgb": {"type": "default", "method": "rgb", "dependencies": [], "parameters": {}},
         # "hsv": {"type": "default", "method": "hsv", "dependencies": [], "parameters": {}},
-        # "dexined": {"type": "edges", "method": "dexined", "dependencies": [], "parameters": {"device": "cuda"}},
+        # "dexined": {"type": "edges", "method": "dexined", "dependencies": [], "parameters": {"device": device}},
         # "softseg gb": {"type": "soft-segmentation", "method": "generalized_boundaries", "dependencies": [],
         #                 "parameters": {"useFiltering": True, "adjustToRGB": True, "maxChannels": 3}},
         # "softseg kmeans": {"type": "soft-segmentation", "method": "kmeans", "dependencies": [],
         #                     "parameters": {"n_clusters": 6, "epsilon": 2, "max_iterations": 10, "attempts": 3}},
-        "canny": {"type": "edges", "method": "canny", "dependencies": [],
-                 "parameters": {"threshold1": 100, "threshold2": 200, "aperture_size": 3, "l2_gradient": True}},
+        # "canny": {"type": "edges", "method": "canny", "dependencies": [],
+        #           "parameters": {"threshold1": 100, "threshold2": 200, "aperture_size": 3, "l2_gradient": True}},
+        "depth dpt": {"type": "depth", "method": "dpt", "dependencies": [], "parameters": {"device": device}},
     }
 
     representations = build_representations_from_cfg(video, representations_dict)
@@ -39,11 +42,11 @@ def test_vre_batched():
     shutil.rmtree(tmp_dir, ignore_errors=True)
     shutil.rmtree(tmp_dir2, ignore_errors=True)
 
-    start_frame, end_frame = 1000, 1100
+    start_frame, end_frame = 1000, 1020
     vre = VRE(video, representations)
     took1 = vre(tmp_dir, start_frame=start_frame, end_frame=end_frame, export_raw=True, export_png=True, batch_size=1)
     vre2 = VRE(video, representations2)
-    took2 = vre2(tmp_dir2, start_frame=start_frame, end_frame=end_frame, export_raw=True, export_png=True, batch_size=20)
+    took2 = vre2(tmp_dir2, start_frame=start_frame, end_frame=end_frame, export_raw=True, export_png=True, batch_size=5)
 
     for representation in vre.representations.keys():
         for t in range(start_frame, end_frame):
