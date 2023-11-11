76795847733d22cb215ef94d6a9b508a164dddf2
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2023-11-11 12:08:26 +0000

    QOL changes

--- test/end_to_end/imgur/run.sh
+++ test/end_to_end/imgur/run.sh
@@ -2,17 +2,30 @@
 
 export FILE_ABS_PATH=$(readlink -f ${BASH_SOURCE[0]})
 export CWD=$(dirname $FILE_ABS_PATH)
+old_cwd=$(pwd)
 
-test -e imgur.sh || wget https://raw.githubusercontent.com/tremby/imgur.sh/main/imgur.sh -O imgur.sh # imgur upload script
-chmod +x imgur.sh
-test -e DJI_0956_720p.MP4 || gdown --no-cookies https://drive.google.com/uc?id=158U-W-Gal6eXxYtS1ca1DAAxHvknqwAk # video
+
+# download stuff
+## imgur upload script
+test -e $CWD/imgur.sh || wget https://raw.githubusercontent.com/tremby/imgur.sh/main/imgur.sh -O $CWD/imgur.sh
+chmod +x $CWD/imgur.sh
+## test video
+test -e $CWD/DJI_0956_720p.MP4 \
+    || gdown --no-cookies https://drive.google.com/uc?id=158U-W-Gal6eXxYtS1ca1DAAxHvknqwAk \
+        -O $CWD/DJI_0956_720p.MP4
+## download weights
 mkdir -p $VRE_WEIGHTS_DIR
-cd $VRE_WEIGHTS_DIR
-test -e  DJI_0956_velocities.npz || gdown https://drive.google.com/uc?id=1yafmmiHGMsgX6ym9Pbx2X2OxJtCY1xrT # velocities
-test -e safeuav_semantic_0956_pytorch.ckpt || gdown https://drive.google.com/uc?id=1Up0pU1PRW0lzOzTEmV-c9eTLTe02kIpe  # semantic net pytorch
+### velocities for odo flow
+test -e $VRE_WEIGHTS_DIR/DJI_0956_velocities.npz || \
+    gdown https://drive.google.com/uc?id=1yafmmiHGMsgX6ym9Pbx2X2OxJtCY1xrT -O $VRE_WEIGHTS_DIR/DJI_0956_velocities.npz
+### weights for safeuav semantic
+test -e $VRE_WEIGHTS_DIR/safeuav_semantic_0956_pytorch.ckpt || \
+    gdown https://drive.google.com/uc?id=1Up0pU1PRW0lzOzTEmV-c9eTLTe02kIpe \
+        -O $VRE_WEIGHTS_DIR/safeuav_semantic_0956_pytorch.ckpt
 
-cd $CWD
-test -f cfg.yaml || ( echo "cfg.yaml does not exist"; kill $$ ) # cfg
+# make sure we start from scratch
+cd $CWD;
+test -f cfg.yaml || ( echo "cfg.yaml does not exist"; kill $$ )
 X=$(shuf -i 1-9000 -n 1)
 rm -rf test_imgur/
 
@@ -26,3 +39,6 @@ test -f $out_img || ( echo "Image $out_img not found"; kill $$ )
 # xdg-open $out_img
 test `which convert` && convert -resize 50% $out_img $out_img
 bash imgur.sh $out_img
+
+# go back to where we were
+cd $old_cwd
