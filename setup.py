69e37dd80637b9baba71fe89edf6ad5d6a7ed7e5
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2024-05-17 08:37:55 +0000

    Release 1.0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ REQUIRED = ["numpy>=1.21.6", "PyYAML==6.0", "pims==0.6.1", "tqdm==4.66.1", "nats
             "torch==2.2.1", "torchvision==0.17.1", "timm==0.6.13", "transforms3d==0.4.1", "pyproj>=3.2.0",
             "overrides==7.3.1", "pandas==2.1.3", "matplotlib==3.7.1", "flow_vis==0.1", "colorama==0.4.6",
             "omegaconf==2.3.0", "lovely_tensors==0.1.15", "fvcore==0.1.5.post20221221", "pycocotools==2.0.7",
-            "cloudpickle==3.0.0", "moviepy==1.0.3", "opencv-python==4.7.0.68", "Pillow==10.3.0", "scipy==1.8.1"]
+            "moviepy==1.0.3", "opencv-python==4.7.0.68", "Pillow==10.3.0", "scipy==1.8.1"]
 
 setup(
     name=NAME,
