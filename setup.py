690666c033d7fcf7f2c17f26ff5180595c374007
Meehai <mihaicristianpirvu@gmail.com>
2024-06-19 08:28:05 +0300

    little update

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ with open(f"{Path(__file__).absolute().parent}/README.md", "r", encoding="utf-8"
     long_description = fh.read()
 
 REQUIRED = ["numpy>=1.21.6,<2.0.0", "PyYAML==6.0", "pims==0.6.1", "tqdm==4.66.1", "natsort==8.2.0", "gdown==4.6.0",
-            "torch>=2.3.1,<3.0.0", "torchvision>=0.18.1,<1.0.0", "timm==0.6.13", "transforms3d==0.4.1", "pyproj>=3.2.0",
-            "overrides==7.3.1", "pandas==2.1.3", "matplotlib==3.7.1", "flow_vis==0.1", "colorama==0.4.6",
+            "torch>=2.2.1,<3.0.0", "torchvision>=0.17.1,<1.0.0", "timm==0.6.13", "transforms3d==0.4.1", "pyproj>=3.2.0",
+            "overrides==7.3.1", "pandas>=2.1.3,<3.0.0", "matplotlib==3.7.1", "flow_vis==0.1", "colorama==0.4.6",
             "omegaconf==2.3.0", "lovely_tensors==0.1.15", "fvcore==0.1.5.post20221221", "pycocotools==2.0.7",
             "moviepy==1.0.3", "opencv-python==4.7.0.68", "Pillow==10.3.0", "scipy==1.8.1"]
 
