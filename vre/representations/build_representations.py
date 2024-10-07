ee684dd7c09ade6293ef0d3fa3372680aa6ce979
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2024-10-07 11:29:54 +0000

    small yaml cfg revamp

--- vre/representations/build_representations.py
+++ vre/representations/build_representations.py
@@ -6,64 +6,52 @@ from ..utils import topological_sort
 from ..representation import Representation
 
 # pylint: disable=import-outside-toplevel, redefined-builtin, too-many-branches, too-many-statements
-def build_representation_type(type: str, name: str) -> Type[Representation]:
+def build_representation_type(repr_type: str) -> Type[Representation]:
     """Gets the representation type from a type and a name (the two identifiers of a representation)"""
     obj_type: Type[Representation] | None = None
+    assert len(repr_type.split("/")) == 2, f"No extra '/' allowed in the type. Got: '{repr_type}'. Correct: depth/dpt"
 
-    if type == "default":
-        if name == "rgb":
-            from .rgb import RGB
-            obj_type = RGB
-        elif name == "hsv":
-            from .hsv import HSV
-            obj_type = HSV
-
-    elif type == "soft-segmentation":
-        if name == "python-halftone":
-            from .soft_segmentation.halftone import Halftone
-            obj_type = Halftone
-        elif name == "generalized_boundaries":
-            from .soft_segmentation.generalized_boundaries import GeneralizedBoundaries
-            obj_type = GeneralizedBoundaries
-        elif name == "fastsam":
-            from .soft_segmentation.fastsam import FastSam
-            obj_type = FastSam
-
-    elif type == "edges":
-        if name == "dexined":
-            from .edges.dexined import DexiNed
-            obj_type = DexiNed
-        elif name == "canny":
-            from .edges.canny import Canny
-            obj_type = Canny
-
-    elif type == "depth":
-        if name == "dpt":
-            from .depth.dpt import DepthDpt
-            obj_type = DepthDpt
-
-    elif type == "optical-flow":
-        if name == "rife":
-            from .optical_flow.rife import FlowRife
-            obj_type = FlowRife
-        elif name == "raft":
-            from .optical_flow.raft import FlowRaft
-            obj_type = FlowRaft
-
-    elif type == "semantic-segmentation":
-        if name == "safeuav":
-            from .semantic_segmentation.safeuav import SafeUAV
-            obj_type = SafeUAV
-        elif name == "mask2former":
-            from .semantic_segmentation.mask2former import Mask2Former
-            obj_type = Mask2Former
-
-    elif type == "normals":
-        if name == "depth-svd":
-            from .normals.depth_svd import DepthNormalsSVD
-            obj_type = DepthNormalsSVD
-
-    assert obj_type is not None, f"Unknown type: {type}, name: {name}"
+    if repr_type == "default/rgb":
+        from .rgb import RGB
+        obj_type = RGB
+    elif repr_type == "default/hsv":
+        from .hsv import HSV
+        obj_type = HSV
+    elif repr_type == "soft-segmentation/python-halftone":
+        from .soft_segmentation.halftone import Halftone
+        obj_type = Halftone
+    elif repr_type == "soft-segmentation/generalized_boundaries":
+        from .soft_segmentation.generalized_boundaries import GeneralizedBoundaries
+        obj_type = GeneralizedBoundaries
+    elif repr_type == "soft-segmentation/fastsam":
+        from .soft_segmentation.fastsam import FastSam
+        obj_type = FastSam
+    elif repr_type == "edges/dexined":
+        from .edges.dexined import DexiNed
+        obj_type = DexiNed
+    elif repr_type == "edges/canny":
+        from .edges.canny import Canny
+        obj_type = Canny
+    elif repr_type == "depth/dpt":
+        from .depth.dpt import DepthDpt
+        obj_type = DepthDpt
+    elif repr_type == "optical-flow/rife":
+        from .optical_flow.rife import FlowRife
+        obj_type = FlowRife
+    elif repr_type == "optical-flow/raft":
+        from .optical_flow.raft import FlowRaft
+        obj_type = FlowRaft
+    elif repr_type == "semantic-segmentation/safeuav":
+        from .semantic_segmentation.safeuav import SafeUAV
+        obj_type = SafeUAV
+    elif repr_type == "semantic-segmentation/mask2former":
+        from .semantic_segmentation.mask2former import Mask2Former
+        obj_type = Mask2Former
+    elif repr_type == "normals/depth-svd":
+        from .normals.depth_svd import DepthNormalsSVD
+        obj_type = DepthNormalsSVD
+    else:
+        raise ValueError(f"Unknown type: '{repr_type}'")
     return obj_type
 
 def build_representation_from_cfg(repr_cfg: dict, name: str, built_so_far: dict[str, Representation]) -> Representation:
@@ -71,15 +59,17 @@ def build_representation_from_cfg(repr_cfg: dict, name: str, built_so_far: dict[
     Builds a representation given a dict config and a name.
     Convention:
     - name: VRE Name aka runtime name or how the directories are going to be called after vre call
-    - repr_cfg["type"] The type of the representation (i.e optical flow, semanatic, depth etc.)
-    - repr_cfg["name"] The name of the type of representation (i.e rife, dpt, m2f etc.)
+    - repr_cfg["type"] The type of the representation (i.e optical-flow/rife, semanatic-segmentation/mask2former,
+        depth/dpt etc.)
+    - parameters The parameters sent to each representation
+    - dependencies The dependencies names based for each representation (can be an empty list)
     """
     assert isinstance(repr_cfg, dict), f"Broken format (not a dict) for {name}. Type: {type(repr_cfg)}."
-    assert {"type", "name", "parameters", "dependencies"}.issubset(repr_cfg), f"{name} wrong keys: {repr_cfg.keys()}"
+    assert {"type", "parameters", "dependencies"}.issubset(repr_cfg), f"{name} wrong keys: {repr_cfg.keys()}"
     assert isinstance(repr_cfg["parameters"], dict), type(repr_cfg["parameters"])
-
-    logger.info(f"Building '{repr_cfg['type']}'/'{repr_cfg['name']}' (vre name: {name})")
-    obj_type = build_representation_type(repr_cfg["type"], repr_cfg["name"])
+    assert len(name.find("/")) == -1, "No '/' allowed in the representation name. Got '{name}'"
+    logger.info(f"Building '{repr_cfg['type']}' (vre name: {name})")
+    obj_type = build_representation_type(repr_cfg["type"])
     dependencies = [built_so_far[dep] for dep in repr_cfg["dependencies"]]
     obj: Representation = obj_type(name=name, dependencies=dependencies, **repr_cfg["parameters"])
 
