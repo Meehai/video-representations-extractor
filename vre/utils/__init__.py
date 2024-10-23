ffefdd73bb5fe00520a213d539478e36109bbe56
meehai <mihaicristianpirvu@gmail.com>
2024-10-23 10:54:08 +0300

    backport

--- vre/utils/__init__.py
+++ vre/utils/__init__.py
@@ -3,7 +3,8 @@ from .utils import (get_project_root, parsed_str_type, took, is_dir_empty, get_c
                     now_fmt, is_git_lfs, semantic_mapper)
 from .vre_video import FakeVideo, VREVideo
 from .topological_sort import topological_sort
-from .image import image_resize, image_resize_batch, image_write, image_read, image_add_title, collage_fn
+from .image import (image_resize, image_resize_batch, image_write, image_read, image_add_title, collage_fn,
+                    image_add_border)
 from .resources import fetch_weights, vre_load_weights, fetch_resource
 from .batches import make_batches, all_batch_exists
 from .colorizer import colorize_depth, colorize_semantic_segmentation, colorize_optical_flow
--- vre/utils/image.py
+++ vre/utils/image.py
@@ -110,3 +110,37 @@ def image_add_title(image: np.ndarray, text: str, font: str = None, font_color: 
     return {"PIL": pil_image_add_title}[library](image=image, text=text, font=font, font_color=font_color,
                                                  size_px=size_px, background_color=background_color,
                                                  top_padding=top_padding)
+
+
+def image_add_border(image: np.ndarray, color: tuple[int, int, int] | int, thicc: int | None,
+                     add_x: bool = False, inplace: bool = False) -> np.ndarray:
+    """
+    Given an image, add rectangles to it on each side. Optionally, cross it with an X (1 line to each diagonal).
+    The original image is not altered unless inplace is set to True.
+    Parameters:
+    - image The image that is bordered.
+    - color The color of the border. Must be a [0:255] tuple.
+    """
+    assert len(image.shape) == 3, f"Wrong image shape: {image.shape}"
+    assert thicc is None or (isinstance(thicc, int) and thicc > 0), thicc
+    color = [color, color, color] if isinstance(color, int) else color
+    assert len(color) == 3, f"Wrong color shape: {color}"
+    h, w = image.shape[0: 2]
+    if thicc is None:
+        logger.debug2(f"Thicc not provided, defaulting to {thicc}, based on {h=} and {w=}.")
+        thicc = max(1, min(7, h // 33)) if h >= w else max(1, min(7, w // 33)) # heuristic, to be changed if it's bad.
+
+    idx = np.linspace(0, 1, max(h, w))
+    x = np.arange(w) if h >= w else np.round(idx * (w - 1)).astype(int)
+    y = np.arange(h) if w >= h else np.round(idx * (h - 1)).astype(int)
+
+    new_image = image if inplace else image.copy()
+    if add_x:
+        for t in range(thicc):
+            new_image[np.clip(y + t, 0, h - 1), np.clip(x, 0, w - 1)] = color
+            new_image[np.clip(h - (y + 1 + t), 0, h - 1), np.clip(x, 0, w - 1)] = color
+    new_image[0: thicc] = color
+    new_image[-thicc:] = color
+    new_image[:, 0: thicc] = color
+    new_image[:, -thicc:] = color
+    return new_image
