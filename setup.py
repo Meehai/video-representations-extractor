42f890219c65e4ddc22c0bf978670655750ee562
meehai <mihaicristianpirvu@gmail.com>
2024-10-23 10:18:01 +0300

    1.1.2 i guess. Do I need pyproj ??

--- setup.py
+++ setup.py
@@ -3,18 +3,36 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 NAME = "video-representations-extractor"
-VERSION = "1.1.1"
+VERSION = "1.1.2"
 DESCRIPTION = "Video Representations Extractor (VRE) for computing algorithmic or neural representations of each frame."
 URL = "https://gitlab.com/video-representations-extractor/video-representations-extractor"
 
 with open(f"{Path(__file__).absolute().parent}/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-REQUIRED = ["numpy>=1.21.6,<2.0.0", "PyYAML==6.0", "pims==0.7.0", "tqdm==4.66.5", "natsort==8.2.0",
-            "torch==2.4.1", "torchvision==0.19.1", "timm==1.0.9", "pyproj>=3.2.0", "overrides==7.7.0",
-            "pandas>=2.1.3,<3.0.0", "matplotlib==3.7.1", "flow_vis==0.1", "loggez==0.4.1", "omegaconf==2.3.0",
-            "lovely_tensors==0.1.17", "fvcore==0.1.5.post20221221", "pycocotools==2.0.7", "moviepy==1.0.3",
-            "opencv-python==4.7.0.68", "Pillow==10.3.0", "diffusers==0.30.3"]
+REQUIRED = [
+    "numpy>=1.21.6,<2.0.0",
+    "PyYAML==6.0",
+    "pims==0.7.0",
+    "tqdm==4.66.5",
+    "natsort==8.2.0",
+    "torch==2.4.1",
+    "torchvision==0.19.1",
+    "overrides==7.7.0",
+    "pandas>=2.1.3,<3.0.0",
+    "matplotlib==3.7.1",
+    "loggez==0.4.1",
+    "omegaconf==2.3.0",
+    "lovely_tensors==0.1.17",
+    "opencv-python==4.7.0.68",
+    "moviepy==1.0.3",
+    "Pillow==10.3.0",
+    "pycocotools==2.0.7",
+    "flow_vis==0.1",
+    "timm==1.0.9",
+    "fvcore==0.1.5.post20221221",
+    "diffusers==0.30.3",
+]
 
 setup(
     name=NAME,
@@ -26,7 +44,7 @@ setup(
     packages=find_packages(),
     install_requires=REQUIRED,
     dependency_links=[],
-    license="WTFPL",
+    license="MIT",
     python_requires=">=3.10",
     scripts=["bin/vre", "bin/vre_collage"],
 )
