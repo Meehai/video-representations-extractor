f0b089236d4223897ae08bc8ab6f2875901be5f4
meehai <mihaicristianpirvu@gmail.com>
2024-10-23 14:19:15 +0300

    changes like speed of lit

--- vre/stored_representation/__init__.py
+++ vre/stored_representation/__init__.py
@@ -1,5 +1,6 @@
 """init file"""
 from .stored_represntation import StoredRepresentation, NpzRepresentation
+from .normed_representation import NormedRepresentation
 from .task_mapper import TaskMapper
 from .cv_representations import (RGBRepresentation, HSVRepresentation, EdgesRepresentation, SemanticRepresentation,
                                  DepthRepresentation, OpticalFlowRepresentation, NormalsRepresentation,
