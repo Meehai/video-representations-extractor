4772458b81f2278fc0535b714b6b7acc50578db2
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2024-10-25 19:04:30 +0000

    DataWriter of a single representation

--- vre/video_representations_extractor.py
+++ vre/video_representations_extractor.py
@@ -44,6 +44,7 @@ class VideoRepresentationsExtractor:
     def _do_one_representation(self, representation: Representation, data_writer: DataWriter,
                                runtime_args: VRERuntimeArgs) -> list[float]:
         """main loop of each representation."""
+        logger.info(f"Running:\n{representation}\n{data_writer}")
         name = representation.name
         batch_size, output_size = runtime_args.batch_sizes[name], runtime_args.output_sizes[name]
         data_storer = DataStorer(data_writer, runtime_args.n_threads_data_storer)
@@ -60,7 +61,7 @@ class VideoRepresentationsExtractor:
         repr_stats: list[float] = []
         pbar = tqdm(total=runtime_args.end_frame - runtime_args.start_frame, desc=f"[VRE] {name} bs={batch_size}")
         for l, r in zip(left, right): # main VRE loop
-            if data_writer.all_batch_exists(representation.name, l, r):
+            if data_writer.all_batch_exists(l, r):
                 pbar.update(r - l)
                 repr_stats.extend(took(datetime.now(), l, r))
                 continue
@@ -79,7 +80,7 @@ class VideoRepresentationsExtractor:
                 imgs = representation.make_images(self.video[l: r], y_repr_rsz) if data_writer.export_image else None
                 if representation.output_dtype is not None:
                     y_repr_rsz.output = y_repr_rsz.output.astype(representation.output_dtype)
-                data_storer(name, y_repr_rsz, imgs, l, r)
+                data_storer(y_repr_rsz, imgs, l, r)
             except Exception:
                 self._log_error(f"\n[{name} {batch_size=} {l=} {r=}] {traceback.format_exc()}\n")
                 repr_stats.extend([1 << 31] * (runtime_args.end_frame - l))
@@ -104,12 +105,11 @@ class VideoRepresentationsExtractor:
         self._setup_logger(output_dir, now := now_fmt())
         runtime_args = VRERuntimeArgs(self.video, self.representations, start_frame, end_frame, batch_size,
                                       exception_mode, output_size, load_from_disk_if_computed, n_threads_data_storer)
-        data_writer = DataWriter(output_dir, [r.name for r in self.representations.values()],
-                                 output_dir_exists_mode=output_dir_exists_mode, binary_format=binary_format,
-                                 image_format=image_format, compress=compress)
-        logger.info(f"{runtime_args}\n{data_writer}")
+        logger.info(runtime_args)
         self._metadata = {"run_stats": {}}
         for name, vre_repr in self.representations.items():
+            data_writer = DataWriter(output_dir, representation=name, output_dir_exists_mode=output_dir_exists_mode,
+                                     binary_format=binary_format, image_format=image_format, compress=compress)
             repr_res = self._do_one_representation(vre_repr, data_writer, runtime_args)
             if repr_res[-1] == 1 << 31 and runtime_args.exception_mode == "stop_execution":
                 raise RuntimeError(f"Representation '{name}' threw. Check '{self._logs_file}' for information")
