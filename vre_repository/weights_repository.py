3b4f1661ac71ca9521d34d3b89fbada9e73bfa73
meehai <mihaicristianpirvu@gmail.com>
2025-02-02 11:14:15 +0200

    small changes

--- vre_repository/weights_repository.py
+++ vre_repository/weights_repository.py
@@ -1,7 +1,6 @@
 """Weights Repository module for Learnable Representations in the default repository"""
 from pathlib import Path
-from vre.utils.utils import is_git_lfs
-from vre.utils.resources import RESOURCES_DIR, fetch_resource
+from vre.utils.resources import fetch_resource
 
 def fetch_weights(paths: list[str], depth: int = 0) -> list[Path]:
     """fetches weights for a representation from the repository (if needed) and returns the local path"""
@@ -12,9 +11,6 @@ def fetch_weights(paths: list[str], depth: int = 0) -> list[Path]:
         if isinstance(path, list): # cases like marigold where 1 ckpt file is split across multiple ckpt files
             local_path = fetch_weights(path, depth + 1)[0].parent
         else:
-            local_path = RESOURCES_DIR / "weights" / path
+            local_path = fetch_resource(f"weights/{path}")
         res.append(local_path)
-        if local_path.exists() and not is_git_lfs(local_path): # is_git_lfs is for CI where weights are lfs links
-            continue
-        fetch_resource(f"weights/{path}")
     return res
