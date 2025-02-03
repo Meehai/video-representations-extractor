8c525e3a7d041f68140a4aaf6208edd3273835eb
meehai <mihaicristianpirvu@gmail.com>
2025-02-03 10:02:36 +0200

    main.py little change

--- examples/vre_manual_representation_run/main.py
+++ examples/vre_manual_representation_run/main.py
@@ -18,19 +18,26 @@ if __name__ == "__main__":
     all_representations_dict = OmegaConf.load(open(Path.cwd() / "cfg.yaml", "r"))
 
     device = "cuda" if tr.cuda.is_available() else "cpu"
-    # depth = Marigold(name="marigold", dependencies=[], variant="marigold-lcm-v1-0", denoising_steps=4, ensemble_size=1,
-    #                  processing_resolution=768)
     ixs = [2303, 3392]
     print(ixs)
-    depth = SimpleNamespace(data=ReprOut(frames=video[ixs], output=MemoryData(np.load("data.npz")["arr_0"]), key=ixs),
-                            make_images=lambda data: (colorize_depth(data.output,
-                                                                     percentiles=[1, 95]) * 255).astype(np.uint8))
+    # decomment this and depth.data/setup/free below if you want to store the depth and work on normals only for example
+    # depth = SimpleNamespace(data=ReprOut(frames=video[ixs], output=MemoryData(np.load("data.npz")["arr_0"]), key=ixs),
+    #                         make_images=lambda data: (colorize_depth(data.output,
+    #                                                                  percentiles=[1, 95]) * 255).astype(np.uint8))
+    depth = Marigold(name="marigold", dependencies=[], variant="marigold-lcm-v1-0", denoising_steps=4, ensemble_size=1,
+                     processing_resolution=768)
+    depth.device = device
     normals = DepthNormalsSVD(name="normals1", dependencies=[depth], sensor_fov=75,
                              sensor_size=(3840, 2160), window_size=11)
 
-    normals.data = None
+    depth.vre_setup()
+    depth.data = None
+    np.random.seed(42)
+    tr.random.manual_seed(42)
+    depth.compute(video, ixs)
+    # np.savez_compressed("data.npz", depth.data.output)
     # depth.data = ReprOut(frames=video[ixs], output=MemoryData(np.load("data.npz")["arr_0"]), key=ixs)
-    # depth.compute(video, ixs)
+    normals.data = None
     normals.compute(video, ixs)
     y_depth_img = depth.make_images(depth.data)
     y_normals_img = normals.make_images(normals.data)
@@ -38,5 +45,3 @@ if __name__ == "__main__":
         res = [image_resize(depth.data.frames[i], *y_depth_img[i].shape[0:2]), y_depth_img[i], y_normals_img[i]]
         image_write(collage_fn(res, rows_cols=(1, -1)), f"res_{ixs[i]}.png")
     # depth.vre_free()
-
-    breakpoint()
