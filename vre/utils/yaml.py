5390a039e96081669ae155b4513e91913f63369f
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2025-08-27 16:02:26 +0300

    VRE yaml updates: support for oc.encode

--- vre/utils/yaml.py
+++ vre/utils/yaml.py
@@ -1,9 +1,41 @@
-"""yaml utils -- a minimum subset of stuff from omegaconf"""
+"""
+yaml utils -- a minimum subset of stuff from omegaconf
+Warning! This yaml syntax is not supported:
+key: {other_key: ${oc.env}}
+Use this instead:
+key:
+  other_key: ${oc.env}
+"""
 from io import IOBase, StringIO
 from pathlib import Path
+import ast
 import os
 from typing import Any
 import yaml
+
+def _parse_atom(v: list | dict | str | int | float | bool) -> list | dict | str | int | float | bool:
+    """parses the config file and resolves environment variables or decodes them if needed. Emulates omegaconf."""
+    assert isinstance(v, (list, dict, str, int, float, bool)), type(v)
+    if isinstance(v, list):
+        return [_parse_atom(_v) for _v in v]
+    if isinstance(v, dict):
+        return {k: _parse_atom(_v) for k, _v in v.items()}
+    if not isinstance(v, str):
+        return v
+    if v.startswith("${oc.env"):
+        assert v.find("{", 9) == -1, f"cannot have embedded oc inside oc.env: {v}"
+        assert v[-1] == "}", f"doesn't end in '}}': {v}"
+        key_and_default = v[9:-1].split(",")
+        assert len(key_and_default) <= 2, (v, key_and_default)
+        key, default = key_and_default[0], None if len(key_and_default) == 1 else key_and_default[1]
+        if default is None and key not in os.environ:
+            raise KeyError(f"Environment variable: '{key}' not set and no default provided.")
+        value = os.getenv(key, default)
+        return value
+    if v.startswith("${oc.decode"):
+        inner = _parse_atom(v[12:-1])
+        return ast.literal_eval(inner)
+    return v
 
 def vre_yaml_load(path: Path | str | IOBase) -> dict[str, Any]:
     """reads a yaml file and resolves the env varaibles if needed"""
@@ -11,27 +43,11 @@ def vre_yaml_load(path: Path | str | IOBase) -> dict[str, Any]:
     fp.seek(0)
     data: str = fp.read()
     assert len(data) > 0, f"Nothing read from '{path}'"
-    skip = 0
-    new_data = ""
-    while (ix := data.find("${oc.env:", skip)) != -1:
-        new_data += data[skip: ix]
-        ix_r = data.find("}", ix+9)
-        if ix_r == -1:
-            raise ValueError(f"Unfinished oc.env: {data[ix:]}")
-        key_and_default = data[ix+9: ix_r].split(",")
-        assert len(key_and_default) <= 2, key_and_default
-        has_default = False
-        key, default = key_and_default[0], None
-        if len(key_and_default) == 2:
-            has_default = True
-            key, default = key_and_default
-        if not has_default and key not in os.environ:
-            raise KeyError(f"Environment variable: '{key}' not set and no default provided.")
-        if "{" in key:
-            raise ValueError("'{' cannot be in the read key: '" + key + "'")
-        read_key = os.getenv(key, default)
-        new_data += read_key
-        skip = ix_r + 1
-    new_data += data[skip:]
-    cfg = yaml.safe_load(StringIO(new_data))
+
+    try:
+        cfg_raw = yaml.safe_load(StringIO(data))
+    except:
+        breakpoint()
+    cfg = _parse_atom(cfg_raw)
+    fp.close() if isinstance(path, (Path, str)) else None
     return cfg
