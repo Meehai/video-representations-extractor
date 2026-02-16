4c24e9ad5688e6d0bffd445897338c99ed4841ec
meehai <mihaicristianpirvu@gmail.com>
2026-02-16 15:25:38 +0200

    fixes and setup bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 NAME = "video-representations-extractor"
-VERSION = "1.15.0"
+VERSION = "1.16.0"
 DESCRIPTION = "Video Representations Extractor (VRE) for computing algorithmic or neural representations of each frame."
 URL = "https://gitlab.com/video-representations-extractor/video-representations-extractor"
 
