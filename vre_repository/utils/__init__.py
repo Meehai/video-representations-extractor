02a8a9270458f54642afac2cfec802609d3f174f
meehai <mihaicristianpirvu@gmail.com>
2026-02-23 09:06:39 +0200

    rm from here

--- vre_repository/utils/__init__.py
+++ vre_repository/utils/__init__.py
@@ -3,4 +3,3 @@ from .weights_repository import fetch_weights, vre_load_weights
 from .colorizer import colorize_depth, colorize_semantic_segmentation, colorize_optical_flow
 from .utils import semantic_mapper
 from .image_old import image_write, image_read, image_add_title, collage_fn, image_add_border, image_blend
-from .image_utils import image_resize
